a2a3f4dc7cdc652f39443c0b80c555686972866c
isaacgichuhi <zacmuturi45@gmail.com>
2023-09-23 13:03:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+function moveDodgerLeft(e) {
+    const rite = dodger.style.left.replace('px', '');
+    const right = parseInt(rite, 10);
+
+if (right > 0) {
+    dodger.style.left = `${right - 5}px`;
+}
+}
+
+function moveDodgerRight(e) {
+    const rite = dodger.style.left.replace('px', '');
+    const right = parseInt(rite, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 5}px`;
+    }
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+})
+
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+})
